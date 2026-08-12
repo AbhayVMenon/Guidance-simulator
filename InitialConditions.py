3d3a849b5123da21d6b9877573d6e3b41897ff4f
AbhayVMenon <abhayvmenon@gmail.com>
2026-08-12 23:49:05 +0400

    Simulator done

--- InitialConditions.py
+++ InitialConditions.py
@@ -4,21 +4,24 @@ from Trajectories import *
 
 #T = target, P = Pursuer
 
+#simulation physics
 dt = 0.01 # time step
-speed_multipier = 1.7 # How much faster P is than T
-target_velocity_mag = 1000
-
-#Pursuer inital state vector
-p0 = np.array([0, 500, 0, (speed_multipier * target_velocity_mag), 0, 0]) #[x0, y0, z0, xdot0, ydot0, zdot0]
-
-# np.random.randint(-1000,1000)
+g = 9.81
 x_start = 1000
 x_range = 10000
 
-#global constants
-g = 9.81
-N = 3
-
-#chosen algorithm
+#target setup
+target_velocity_mag = 1000
+t0 = np.array([x_start, 0, 0, target_velocity_mag, 0, 0])
 traj = Helix
-law = AugPropNav
+
+#pursuer setup
+speed_multipier = 1.7 # How much faster P is than T
+N = 3
+max_gs = 40
+kill_zone_size = 25 #effectuve size of missile
+p0 = np.array([0, 500, 0, (speed_multipier * target_velocity_mag), 0, 0]) #[x0, y0, z0, xdot0, ydot0, zdot0]
+
+#Guidance law you want Pursuer to follow
+law_params = {"N": N, "dt": dt, "max_gs": max_gs} #edit only if a guidance law is being added!
+law_name = "APN"
--- Simulator.py
+++ Simulator.py
@@ -2,27 +2,44 @@ from InitialConditions import *
 from GuidanceLaws import State
 import numpy as np
 
-chosen_trajectory = traj(x, x_start, dt, target_velocity_mag)
-
-def target_states_values(dt, x_range, x_start, target_velocity_mag, chosen_trajectory):
+#cretae target trajectory
+def target_states_values(dt, x_range, x_start, target_velocity_mag, traj):
 
     x = np.arange(x_start, x_range + dt * target_velocity_mag, dt * target_velocity_mag)
 
-    y, z, vx, vy, vz = chosen_trajectory(x, x_start)
+    y, z, vx, vy, vz = traj(x, x_start, dt, target_velocity_mag)
 
-    return x, y, z, vx, vy, vz
+    return x, y, z, vx, vy, vz 
 
-t_traj = target_states_values(dt, x_range, x_start, target_velocity_mag, traj) #outputs position and velocity of target
+t_x, t_y, t_z, t_vx, t_vy, t_vz = target_states_values(dt, x_range, x_start, target_velocity_mag, traj) #outputs position and velocity of target
+
+#initialising pursuer vector
+p_post = np.zeros(len(t_x)-1) 
+
+p_x = np.insert(p_post, 0, p0[0])   # position values
+p_y = np.insert(p_post, 0, p0[1])
+p_z = np.insert(p_post, 0, p0[2])
+
+p_vx = np.insert(p_post, 0, p0[3]) # velocity values
+p_vy = np.insert(p_post, 0, p0[4])
+p_vz = np.insert(p_post, 0, p0[5])
 
 
-chosen_law = law(N, dt)
+#finding the law that user has chosen
+law_registry = {"PP": (PurePursuit, ["max_gs"]), 
+                "PN": (PropNav, ["N"]),
+                "APN":(AugPropNav, ["N", "dt"])}
+
+law_class, parameters = law_registry[law_name]
+law_kwargs = {k: law_params[k] for k in parameters}
+chosen_law = law_class(**law_kwargs)
 
 def ac_into_velocity_and_position(p_state, dt, a_c):
 
     p_current_r = p_state.r
 
     p_current_v = p_state.v
-    
+
     p_velocity_new_raw = p_current_v + a_c * dt
 
     unit_p_velocity_new = p_velocity_new_raw / np.linalg.norm(p_velocity_new_raw)
@@ -32,3 +49,31 @@ def ac_into_velocity_and_position(p_state, dt, a_c):
     p_r_new = p_current_r + p_velocity_new * dt
 
     return State(p_r_new, p_velocity_new)
+
+intercept_index = None
+
+for k in range(0, len(t_x) - 1):
+
+    t_state = State(r=np.array([t_x[k], t_y[k], t_z[k]]), v=np.array([t_vx[k], t_vy[k], t_vz[k]]))
+
+    p_state = State(r=np.array([p_x[k], p_y[k], p_z[k]]), v=np.array([p_vx[k], p_vy[k], p_vz[k]]))
+
+    a_c = chosen_law.compute_command(p_state, t_state)
+
+    new_p_state = ac_into_velocity_and_position(p_state, dt, a_c)
+
+    p_x[k+1], p_y[k+1], p_z[k+1] =  new_p_state.r
+    p_vx[k+1], p_vy[k+1], p_vz[k+1] = new_p_state.v
+
+    #check for conditionals at k+1
+    p_t_dist = np.sqrt((t_x[k+1] - p_x[k+1])**2 + (t_y[k+1] - p_y[k+1])**2 + (t_z[k+1] - p_z[k+1])**2)
+
+    if p_t_dist < kill_zone_size: 
+        intercept_index = k + 1
+        break
+
+if intercept_index is None:
+    print("Unsuccessful interception")
+else:
+    intercept_time = intercept_index * dt
+    print(f"Intercept occurs {intercept_time}s after launch")
